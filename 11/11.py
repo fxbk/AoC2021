788209e7e14da2233972867a4e6342bcc72f3991
fxbk <44337498+fxbk@users.noreply.github.com>
2022-02-04 14:22:59 +0100

    Solution day 11 part 1

---
+++ 11/11.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+file = open('input.txt', 'r')
+input = file.read().split('\n')
+input = [[int(char) for char in line] for line in input]
+
+energy_levels = np.matrix(input)
+
+flashes = 0
+for i in range(100):
+    energy_levels += 1
+    flash_idx = np.where(energy_levels > 9)
+    prev_flashes = []
+    while len(flash_idx[0]) != 0:
+        for idx, idy in zip(flash_idx[0], flash_idx[1]):
+            if (idx, idy) not in prev_flashes:
+                flashes += 1
+            energy_levels[idx, idy] = 0
+            if idx > 0:
+                if (idx - 1, idy) not in prev_flashes:
+                    energy_levels[idx - 1, idy] += 1
+                if idy > 0 and (idx - 1, idy - 1) not in prev_flashes:
+                    energy_levels[idx - 1, idy - 1] += 1
+                if idy < energy_levels.shape[1] - 1 and (idx - 1, idy + 1) not in prev_flashes:
+                    energy_levels[idx - 1, idy + 1] += 1
+            if idx < energy_levels.shape[0]-1:
+                if (idx + 1, idy) not in prev_flashes:
+                    energy_levels[idx + 1, idy] += 1
+                if idy > 0 and (idx + 1, idy - 1) not in prev_flashes:
+                    energy_levels[idx + 1, idy - 1] += 1
+                if idy < energy_levels.shape[1]-1 and (idx + 1, idy + 1) not in prev_flashes:
+                    energy_levels[idx + 1, idy + 1] += 1
+            if idy > 0 and (idx, idy - 1) not in prev_flashes:
+                energy_levels[idx, idy - 1] += 1
+            if idy < energy_levels.shape[1]-1 and (idx, idy + 1) not in prev_flashes:
+                energy_levels[idx, idy + 1] += 1
+            prev_flashes.append((idx, idy))
+        flash_idx = np.where(energy_levels > 9)
+
+print(f'Solution part 1: {flashes}')
